6b4a482edf8c1c79a778b818930f3861a08e272f
unknown <kingsoft@Rongxq-PC.(none)>
2014-07-31 10:12:53 +0800

    add baseHandler

--- ajax_server.py
+++ ajax_server.py
@@ -4,89 +4,123 @@ import tornado.ioloop
 import tornado.web
 import json
 
-class LoginHandler(tornado.web.RequestHandler):
+from db import DB
+
+db = DB("my.db")
+
+
+class BaseHandler(tornado.web.RequestHandler):
+	def prepare(self):
+		print("In prepare(), checking current user\n")
+		if not self.current_user:
+			self.redirect("/")
+			return
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+class LoginHandler(BaseHandler):
 	def get(self):
 		self.render('login.html')
-	
-		
-class LogoutHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render('HealthHelper.html')
-
-class RegisterHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render('register.html')
-		
-class RegisterCheckHandler(tornado.web.RequestHandler):
-	def get(self):
-		user = self.get_argument('user');
-		pw = self.get_argument('password');
-		id = "1245"
-		self.write(json.dumps(id))
-
-		
-class HealthHelperHandler(tornado.web.RequestHandler):
-	def get(self):
+	def post(self):
 		id = self.get_argument('id')
 		pw = self.get_argument('password')
-		print id
-		print pw
-		if id == '1245' and pw == '111111':
-			self.render('HealthHelper.html')
+		print( (id,pw) )
+		if str(db.selectPasswordById(id)) == str(pw):
+			self.set_secure_cookie("user", id)
+			self.set_secure_cookie("password", pw)
+			self.redirect("/healthHelper")
 		else:
 			self.redirect('/')
 		
-class PlanDoneHandler(tornado.web.RequestHandler):
+class LogoutHandler(BaseHandler):
+	def post(self):
+		self.set_secure_cookie("user", "")
+		self.redirect('/')
+
+class RegisterHandler(BaseHandler):
+	def post(self):
+		self.render('register.html')
+		
+class RegisterCheckHandler(BaseHandler):
 	def get(self):
-		#data = [["2014-06-11 20:00","2014-06-11 20:30","内环", "running"], ["2014-06-13 20:00","2014-06-13 20:30","内环", "running"]]
-		data = ''
+		#user和password是用户注册时记录的
+		user = self.get_argument('user');
+		pw = self.get_argument('password');
+		newid = db.insertUserTable(None, pw, user, json.dumps(""))
+		print("in RegisterCheckHandler", newid)
+		self.write(json.dumps(newid))
+			
+class HealthHelperHandler(BaseHandler):
+	def get(self):
+		self.render('HealthHelper.html')
+		
+class PlanDoneHandler(BaseHandler):
+	def get(self):
+		data = [{'sportType':'跑步','place':'操场','st':"yyyy-mm-dd HH:MM:SS",'et':"yyyy-mm-dd HH:MM:SS" },{'sportType':'跑步','place':'操场','st':"yyyy-mm-dd HH:MM:SS",'et':"yyyy-mm-dd HH:MM:SS" }]
+		
 		self.write(json.dumps(data))
 		
-class PlanUndoneHandler(tornado.web.RequestHandler):
+class PlanUndoneHandler(BaseHandler):
 	def get(self):
-		data = [["2014-07-11 20:00","2014-07-11 20:30","内环", "running"], ["2014-07-13 20:00","2014-07-13 20:30","内环", "running"]]
+		#data = [{'sportType':'跑步','place':'操场','st':"yyyy-mm-dd HH:MM:SS",'et':"yyyy-mm-dd HH:MM:SS" },{'sportType':'跑步','place':'操场','st':"yyyy-mm-dd HH:MM:SS",'et':"yyyy-mm-dd HH:MM:SS" }]
+		data = ''
 		self.write(json.dumps(data))
 
 #因为在js中已经检查过时间的正确性，所以在这里只需要把新计划放入数据库即可。
-class CreatePlanHandler(tornado.web.RequestHandler):
+class CreatePlanHandler(BaseHandler):
 	def get(self):
-		begin = self.get_argument("begin")
+		st = self.get_argument("st")
+		print st
 		self.write(json.dumps("Create succeed!"))
 		
-class RecordWeekHandler(tornado.web.RequestHandler):
+class RecordWeekHandler(BaseHandler):
 	def get(self):
-		data = [["2014-07-11 20:00","2014-07-11 20:30","内环", 17, 20, 30], ["2014-07-13 20:00","2014-07-13 20:30","内环", 20, 25 ,33]]
+		data = [{'sportType':'跑步', 'place':'操场','st':"yyyy-mm-dd HH:MM:SS", 'et':"yyyy-mm-dd HH:MM:SS", 'sportTime':600,'calorie':10000,'distance':1000},{'sportType':'跑步', 'place':'操场','st':"yyyy-mm-dd HH:MM:SS", 'et':"yyyy-mm-dd HH:MM:SS", 'sportTime':600,'calorie':10000,'distance':1000}]
 		self.write(json.dumps(data))
 		
-class RecordMonthHandler(tornado.web.RequestHandler):
+class RecordMonthHandler(BaseHandler):
 	def get(self):
-		data = [["2014-06-11 20:00","2014-06-11 20:30","内环", 17,20,30], ["2014-06-13 20:00","2014-06-13 20:30","内环", 20, 25, 33]]
+		data = [{'sportType':'跑步', 'place':'操场','st':"yyyy-mm-dd HH:MM:SS", 'et':"yyyy-mm-dd HH:MM:SS", 'sportTime':600,'calorie':10000,'distance':1000},{'sportType':'跑步', 'place':'操场','st':"yyyy-mm-dd HH:MM:SS", 'et':"yyyy-mm-dd HH:MM:SS", 'sportTime':600,'calorie':10000,'distance':1000}]
 		self.write(json.dumps(data))
 		
-class InvitProHandler(tornado.web.RequestHandler):
+		
+		
+class AddRecordHandler(tornado.web.RequestHandler):
 	def get(self):
-		data = [["2014-06-11 20:00","内环", "running", "Lily", ["Join", "Mary"]], ["2014-06-13 20:00","内环", "running", "Tom", ["Henrry"]]]
+		print("AddRecordHandler not done!")
+		self.write( json.dumps("AddRecordHandler not done!") )
+		
+		
+				
+class InvitProHandler(BaseHandler):
+	def get(self):
+		data = [{'sportType':'跑步', 'place':'操场','st':"yyyy-mm-dd HH:MM:SS", 'et':"yyyy-mm-dd HH:MM:SS",'invitee':[1,2,3]},{'sportType':'跑步', 'place':'操场','st':"yyyy-mm-dd HH:MM:SS", 'et':"yyyy-mm-dd HH:MM:SS",'invitee':[1,2,3]}]
+		print data
 		self.write(json.dumps(data))
 		
-class InvitPendHandler(tornado.web.RequestHandler):
+class InvitPendHandler(BaseHandler):
 	def get(self):
-		data = [["2014-07-11 20:00","内环", "running", "Lily", ["Join", "Mary"]], ["2014-07-13 20:00","内环", "running", "Tom", ["Henrry"]]]
+		data = [{'sportType':'跑步', 'place':'操场','st':"yyyy-mm-dd HH:MM:SS", 'et':"yyyy-mm-dd HH:MM:SS",'invitee':[1,2,3]}]
 		self.write(json.dumps(data))
 		
-class SendInvitHandler(tornado.web.RequestHandler):
+class SendInvitHandler(BaseHandler):
 	def get(self):
-		print self.get_argument("invitees");
+		print self.get_argument("invitee");
 		self.write(json.dumps("Invitation has been sent"));
 		
-class GetFriendListHandler(tornado.web.RequestHandler):
+class GetFriendListHandler(BaseHandler):
 	def get(self):
-		data = [{'id':'1', 'name':'Jeny'}, {'id':'2', 'name': 'Tom'},
-		{'id':'3', 'name':'Jeny'},{'id':'4', 'name':'Jeny'},{'id':'5', 'name':'Jeny'},{'id':'6', 'name':'Jeny'},{'id':'7', 'name':'Jeny'}]
-		#data = ''
+		#data = [{'id':'1', 'name':'Jeny'}, {'id':'2', 'name': 'Tom'},
+		#{'id':'3', 'name':'Jeny'},{'id':'4', 'name':'Jeny'},{'id':'5', #'name':'Jeny'},{'id':'6', 'name':'Jeny'},{'id':'7', 'name':'Jeny'}]
+		me = self.current_user
+		print ("In GetFriendListHandler", me)
+		data = db.getFriendInfoById(me)
 		self.write(json.dumps(data))
 	
-class AddFriendHandler(tornado.web.RequestHandler):
+class AddFriendHandler(BaseHandler):
 	def get(self):
+		me = self.get_argument('me')
+		print me
 		id = self.get_argument("id");
 		print id
 		data = [{'id': id, 'name':'hello'}]
@@ -95,7 +129,7 @@ class AddFriendHandler(tornado.web.RequestHandler):
 		#如果添加的好友不存在，则返回空串
 		#self.write(json.dumps(""))
 		
-class DelFriendHandler(tornado.web.RequestHandler):
+class DelFriendHandler(BaseHandler):
 	def get(self):
 		id = self.get_argument("id");
 		print id
@@ -106,12 +140,13 @@ app = tornado.web.Application(
 (r"/logout", LogoutHandler),
 (r"/register", RegisterHandler),
 (r"/registerCheck", RegisterCheckHandler),
-
 (r"/healthHelper", HealthHelperHandler),
+
 (r"/planDone", PlanDoneHandler),
 (r"/planUndone", PlanUndoneHandler),
-(r"/recordWeek", RecordMonthHandler),
+(r"/recordWeek", RecordWeekHandler),
 (r"/recordMonth", RecordMonthHandler),
+
 (r"/invitPro", InvitProHandler),
 (r"/invitPend", InvitPendHandler),
 (r"/createPlan", CreatePlanHandler),
@@ -126,12 +161,14 @@ app = tornado.web.Application(
 (r"/ios/logout", LogoutHandler),
 (r"/ios/register", RegisterHandler),
 (r"/ios/registerCheck", RegisterCheckHandler),
-
 (r"/ios/healthHelper", HealthHelperHandler),
+
 (r"/ios/planDone", PlanDoneHandler),
 (r"/ios/planUndone", PlanUndoneHandler),
 (r"/ios/recordWeek", RecordMonthHandler),
 (r"/ios/recordMonth", RecordMonthHandler),
+(r"/ios/addRecord", AddRecordHandler),
+
 (r"/ios/invitPro", InvitProHandler),
 (r"/ios/invitPend", InvitPendHandler),
 (r"/ios/createPlan", CreatePlanHandler),
@@ -141,6 +178,7 @@ app = tornado.web.Application(
 (r"/ios/delFriend", DelFriendHandler),
 
 ],
+cookie_secret="61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=" ,
 template_path = os.path.join(os.path.dirname(__file__), "template"),
 static_path = os.path.join(os.path.dirname(__file__), "static"),
 debug=True
